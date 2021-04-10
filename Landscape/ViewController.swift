88901d73096cdf1e38271e4c5fa89f6805a59494
ucwealth <ucheokeke16@gmail.com>
2021-04-10 22:59:38 +0100

    Revert "A beautiful view"
    This reverts commit bc095cbf772ef2b63b49275d9c0e6aeac920bd5f.

---
+++ Landscape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Landscape
+//
+//  Created by Decagon on 10/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
